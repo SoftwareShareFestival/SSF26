3a2c0aee95df9f9b9347ef2590752f2ef11807f0
wlqrksmswnd <wlqrksmswnd@gmail.com>
2026-06-04 22:00:02 +0900

    campschedule component added to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,12 +3,14 @@ import FestivalIntro from "../components/intro/FestivalIntro";
 import CampPageContent from "../components/camp/CampPageContent";
 import QnAPageContent from "../components/qna/QnAPageContent";
 import Footer from "../components/Footer.jsx";
+import CapmSchedule from "@/components/camp/CampSchedule";
 export default function HomePage() {
 	return (
 		<>
 			<HomePageContent />
 			<FestivalIntro />
 			<CampPageContent />
+			<CapmSchedule />
 			<QnAPageContent />
 			<Footer />
 		</>
