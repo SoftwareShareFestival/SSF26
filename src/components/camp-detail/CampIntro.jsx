9233674ba8f40cda202f7d644b62e907fa24f51b
wlqrksmswnd <wlqrksmswnd@gmail.com>
2026-07-19 13:59:52 +0900

    detail json fixed

--- src/components/camp-detail/CampIntro.jsx
+++ src/components/camp-detail/CampIntro.jsx
@@ -1,2 +1,9 @@
-function CampIntro({ detail }) {}
+function CampIntro({ detail }) {
+	return (
+		<div className="max-w-[var(--content-max)]">
+			<img src={detail.cover} />
+			{detail.cover}
+		</div>
+	);
+}
 export default CampIntro;
